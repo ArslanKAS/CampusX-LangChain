de1531b3371e80297c762e6a6253debe020c1d0e
Arsalan Ali <arslanchaos@gmail.com>
2025-04-27 04:01:35 +0500

    pypdf loader

---
+++ Langchain Document Loaders/pdf_loader.py
@@ -0,0 +1,30 @@
+# Creating a text loader for Langchain Document Loaders
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import PyPDFLoader
+load_dotenv()
+
+loader = PyPDFLoader("Langchain Document Loaders/Arsalan_Ali_Resume_2025.pdf")
+
+docs = loader.load()
+
+print(f"Number of documents: {len(docs)}")  # Print the number of documents loaded
+print(f"Document type: {type(docs)}")  # Print the type of the documents loaded
+print(f"Document type: {type(docs[0])}")  # Print the type of the first document
+print(docs[0].page_content)  # Print the content of the first document
+print(docs[0].metadata)  # Print the metadata of the first document
+
+model = ChatOpenAI(model="gpt-4o-mini")
+prompt = PromptTemplate(
+    template='Write a 2-line summary of the following text: {text}',
+    input_variables=['text']
+)
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+result = chain.invoke({'text': docs[0].page_content})
+print(result)
+
